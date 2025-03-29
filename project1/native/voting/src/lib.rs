8a4c0acbe7c5df8b85c7e32fa91f5d35efc55aba
ulieth <uliana.eth@proton.me>
2025-03-29 15:32:49 +0300

    solana hello world

--- project1/native/voting/src/lib.rs
+++ project1/native/voting/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use solana_program::{
+  account_info::AccountInfo,
+  entrypoint,
+  entrypoint::ProgramResult,
+  pubkey::Pubkey,
+  msg,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// declare and export the program's entrypoint
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+// program entrypoint's implementation
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8]
+) -> ProgramResult {
+    // log a message to the blockchain
+    msg!("Hello, world!");
+
+    // gracefully exit the program
+    Ok(())
 }
