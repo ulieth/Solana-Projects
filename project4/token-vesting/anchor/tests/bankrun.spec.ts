4641089bec3589005c731f16f4cb6b6727655da8
ulieth <uliana.eth@proton.me>
2025-04-07 18:47:45 +0300

    update bankrun test environment

--- project4/token-vesting/anchor/tests/bankrun.spec.ts
+++ project4/token-vesting/anchor/tests/bankrun.spec.ts
@@ -7,6 +7,8 @@ import { SYSTEM_PROGRAM_ID } from '@coral-xyz/anchor/dist/cjs/native/system';
 import { BankrunProvider } from 'anchor-bankrun';
 import { Program } from '@coral-xyz/anchor';
 import { Tokenvesting } from '../target/types/tokenvesting';
+import { createMint } from 'spl-token-bankrun';
+import NodeWallet from '@coral-xyz/anchor/dist/cjs/nodewallet';
 
 describe("Vesting Smart Contract Tests", () => {
 
@@ -16,12 +18,20 @@ describe("Vesting Smart Contract Tests", () => {
   let program: Program<Tokenvesting>;
   let banksClient: BanksClient;
   let employer: Keypair;
+  let mint: PublicKey;
+  let beneficiaryProvider : BankrunProvider;
+  let program2: Program<Tokenvesting>;
+  let vestingAccountKey: PublicKey;
+  const companyName = "companyName";
+  let treasuryTokenAccount: PublicKey;
+  let employeeAccount: PublicKey;
+
   beforeAll(async () => {
     beneficiary = new anchor.web3.Keypair();
     // Start a bankrun in an Anchor workspace, with all the workspace programs deployed.
     // This will spin up a BanksServer and a BanksClient, deploy programs and add accounts as instructed.
     context  = await startAnchor( "", [
-        {name: "tokenvesting", programId: new PublicKey(IDL.address) } // params for the added programs
+        {name: "tokenvesting", programId: new PublicKey(IDL.address) } // the smart contract we're deploying
       ],
       [
         {
@@ -43,7 +53,43 @@ describe("Vesting Smart Contract Tests", () => {
 
     banksClient = context.banksClient;
 
-    employer = context.wallet.payer;
+    employer = provider.wallet.payer;
+
+    mint = await createMint(
+      banksClient,
+      employer,
+      employer.publicKey,
+      null,
+      2, // decimals
+    );
+
+    beneficiaryProvider = new BankrunProvider(context);
+    beneficiaryProvider.wallet = new NodeWallet(beneficiary);
+
+    program2 = new Program<Tokenvesting>(IDL as Tokenvesting, beneficiaryProvider);
+
+    // Deriving PDAs
+    [vestingAccountKey] = PublicKey.findProgramAddressSync(
+      [Buffer.from(companyName)],
+      program.programId
+    );
+    [treasuryTokenAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("vesting_treasury"), Buffer.from(companyName)],
+      program.programId
+    );
+    [employeeAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("employee_vesting"), beneficiary.publicKey.toBuffer(), vestingAccountKey.toBuffer()],
+      program.programId
+    );
 
   });
+
+
+
+
+
+
+
+
+
 });
